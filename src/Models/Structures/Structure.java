e2a52e5adc91c8e0e33b3701fb1e407dc597d48e
Pandi Oldenzeel <pandi.oldenzeel@hotmail.nl>
2020-03-05 23:21:24 +0100

    Setup of new design
    -Setup of new models package
    -Setup of improved structures such as houses, properties, streets, towns and gates.

---
+++ src/Models/Structures/Structure.java
@@ -0,0 +1,17 @@
+package Models.Structures;
+
+import Models.District;
+import Models.Street;
+import Models.Town;
+
+public class Structure 
+{
+	protected int id;
+	protected String name;
+	protected Street street;
+	protected int streetNumber;
+	protected Town town;
+	protected District district;
+	
+	
+}
